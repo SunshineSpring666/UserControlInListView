1ac98805a557dc976b2321b7f352adf79a91e3d8
xuchi <xuchi0601@qq.com>
2023-05-08 15:16:17 +0800

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MaApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public Card SingleCard { get; set; } = new Card();
+        public ObservableCollection<Card> MultipleCards { get; set; } = new() { new Card(), new Card() };
+    }
+
+    public class Card
+    {
+        public string CardTitle { get; set; } = "The Card's Title";
+    }
+
+}
